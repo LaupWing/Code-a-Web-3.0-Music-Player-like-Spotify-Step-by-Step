e25b2f8e918017071089675b7a481433d943b98d
LaupWing <loc_strict@hotmail.com>
2022-04-11 15:31:19 +0200

    constructor finished

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -6,4 +6,33 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable{
    string public baseURI = "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+   string public baseExtensions = ".json";
+   address public artist;
+   uint256 public royaltyFee;
+
+   struct MarketItem{
+      uint256 tokenId;
+      address payable seller;
+      uint256 price;
+   }
+
+   MarketItem[] public marketItems;
+
+   constructor(
+      uint256 _royaltyFee,
+      address _artist,
+      uint256[] memory _prices
+   ) payable{
+      require(
+         _prices.length * _royaltyFee <= msg.value,
+         "Deployer must pay royalty fee fo each token listed on the marketplace"
+      );
+      royaltyFee = _royaltyFee;
+      artist = _artist;
+      for (uint8 i =0; i < _prices.length; i++){
+         require(_prices[i] > 0, "Price must be greater than 0");
+         _mint(address(this), i);
+         marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+      }
+   }
 }
